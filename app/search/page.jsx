e9b5967867901e0030d2dae5c1e14bbcafbc9b56
QurayshSaif <saifquraysh@gmail.com>
2023-08-29 12:18:27 -0400

    testing

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,62 +1,62 @@
-// "use client";
-// import Header from "@/components/Header";
-// import { format } from "date-fns";
-// import { useSearchParams } from "next/navigation";
-// import searchResults from "../../data/searchResults.json";
-// import InfoCard from "../../components/InfoCard";
-// import Map from "@/components/Map";
-// import Footer from "@/components/Footer";
+"use client";
+import Header from "@/components/Header";
+import { format } from "date-fns";
+import { useSearchParams } from "next/navigation";
+import searchResults from "../../data/searchResults.json";
+import InfoCard from "../../components/InfoCard";
+import Map from "@/components/Map";
+import Footer from "@/components/Footer";
 
-// export default function Search() {
-//   const query = useSearchParams();
-//   const location = query.get("location");
-//   const startDate = query.get("startDate");
-//   const endDate = query.get("endDate");
-//   const noOfGuests = query.get("noOfGuests");
+export default function Search() {
+  const query = useSearchParams();
+  const location = query.get("location");
+  const startDate = query.get("startDate");
+  const endDate = query.get("endDate");
+  const noOfGuests = query.get("noOfGuests");
 
-//   const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
-//   const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
-//   const range = `${formattedStartDate} - ${formattedEndDate}`;
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
 
-//   return (
-//     <div>
-//       <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
-//       <main className="flex">
-//         <section className="flex-grow pt-14 px-6">
-//           <p className="text-xs">
-//             300+ stays from {range} for {noOfGuests} guests'
-//           </p>
-//           <h1 className="text-3xl font-semibold mt-2 mb-6">
-//             Stays in {location}
-//           </h1>
-//           <div className="hidden md:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
-//             <p className="button">Cancellation Flexibility</p>
-//             <p className="button">Type of Place</p>
-//             <p className="button">Price</p>
-//             <p className="button">More Filters</p>
-//           </div>
-//           <div className="flex flex-col">
-//             {searchResults.map(
-//               ({ img, location, title, description, star, price, total }) => (
-//                 <InfoCard
-//                   key={img}
-//                   img={img}
-//                   location={location}
-//                   title={title}
-//                   description={description}
-//                   star={star}
-//                   price={price}
-//                   total={total}
-//                 />
-//               )
-//             )}
-//           </div>
-//         </section>
-//         {/* <section className="hidden xl:inline-flex xl:min-w-[600px]">
-//           <Map searchResults={searchResults} />
-//         </section> */}
-//       </main>
-//       <Footer />
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
+      <main className="flex">
+        {/* <section className="flex-grow pt-14 px-6">
+          <p className="text-xs">
+            300+ stays from {range} for {noOfGuests} guests'
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
+          <div className="hidden md:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            <p className="button">Cancellation Flexibility</p>
+            <p className="button">Type of Place</p>
+            <p className="button">Price</p>
+            <p className="button">More Filters</p>
+          </div>
+          <div className="flex flex-col">
+            {searchResults.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section> */}
+        {/* <section className="hidden xl:inline-flex xl:min-w-[600px]">
+          <Map searchResults={searchResults} />
+        </section> */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
